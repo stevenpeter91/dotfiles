c26123b40db962a95b5168ae81b304f69c779d3d
Steven Peter <>
2019-11-06 18:45:17 +0100

    Renamed some files and cleared zshrc

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-export SRC_DIR=$(pwd)
+export DOTFILES=~/.dotfiles
+export SRC_DIR=$DOTFILES
 
-ln -s $SRC_DIR/.zshrc ~/.zshrc
-ln -s $SRC_DIR/.zshenv ~/.zshenv
+ln -s $SRC_DIR/zsh/zshrc ~/.zshrc
+ln -s $SRC_DIR/zsh/zshenv ~/.zshenv
 
-ln -s $SRC_DIR/.hyper.js ~/.hyper.js
+ln -s $SRC_DIR/hyper/hyper.js ~/.hyper.js
 
-ln -s $SRC_DIR/.gitconfig ~/.gitconfig
+ln -s $SRC_DIR/git/gitconfig ~/.gitconfig
 
-ln -s $SRC_DIR/.tmux.conf ~/.tmux.conf
+ln -s $SRC_DIR/tmux/tmux.conf ~/.tmux.conf
 
 ln -s $SRC_DIR/nvim ~/.config/nvim
 ln -s $SRC_DIR/colorls ~/.config/colorls
