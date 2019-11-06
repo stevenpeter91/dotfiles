5d3c9570c4f29d4a2c6069015b5f62a1eba39647
Steven Peter <>
2019-11-06 18:48:19 +0100

    Remove before install.sh creating symlinks

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 export SRC_DIR=$(pwd)
 
+rm -f ~/.zshrc ~/.zshenv ~/.hyper.js ~/.tmux.conf ~/.gitconfig ~/.config/nvim ~/.config/colorls
+
 ln -s $SRC_DIR/.zshrc ~/.zshrc
 ln -s $SRC_DIR/.zshenv ~/.zshenv
 
