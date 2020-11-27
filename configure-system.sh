86d469d9cfa2249697d9ad932c15784863512e88
Steven Peter <526473+gb96@users.noreply.github.com>
2020-11-27 17:02:32 +0100

    - Configured polybar font - Configured terminator - Added font config

--- configure-system.sh
+++ configure-system.sh
@@ -132,6 +132,7 @@ rm -rf ~/.config/rofi && ln -s $SRC_DIR/rofi ~/.config/rofi
 rm -rf ~/.config/bspwm && ln -s $SRC_DIR/bspwm ~/.config/bspwm
 rm -rf ~/.config/sxhkd && ln -s $SRC_DIR/sxhkd ~/.config/sxhkd
 rm -rf ~/.config/polybar && ln -s $SRC_DIR/polybar ~/.config/polybar
+rm -rf ~/.config/terminator && ln -s $SRC_DIR/terminator ~/.config/terminator
 rm -rf ~/.colors && ln -s $SRC_DIR/colors ~/.colors
 
 os=$(uname -a | awk '{ print $4 }')
